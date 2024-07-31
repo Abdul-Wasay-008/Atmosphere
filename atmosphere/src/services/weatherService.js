02ce26836638ae47555a6b663b2453820f4945af
Abdul Wasay <wasay7757@gmail.com>
2024-07-31 13:49:54 +0500

    Fix the security issue regarding the API Key

--- atmosphere/src/services/weatherService.js
+++ atmosphere/src/services/weatherService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '9f05d72e6c2bfc8e194b0c9fac53dff7'; // OpenWeatherMap API key
+const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY; // OpenWeatherMap API key
 const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export const getWeatherByCity = async (city) => {
@@ -18,3 +18,5 @@ export const getWeatherByCity = async (city) => {
         throw error;
     }
 };
+console.log('API Key:', process.env.REACT_APP_OPENWEATHER_API_KEY);
+
